7b0693a02522eb75fb53a1655dfcce6993c7323d
loopByte <milea.vasile959@gmail.com>
2014-02-09 20:54:32 +0200

    Title and description retrieved from database

--- index.php
+++ index.php
@@ -1,24 +1,30 @@
 <?php
 
 /**
- * @author MacTavish
+ * @author loopByte
  * @copyright 2014
  */
     
     require_once 'classes/db.php';
     require_once 'classes/article.php';
+    require_once 'classes/site.php';
 
 ?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Simple CMS</title>
+        <title><?php Site::title(); ?> - <?php Site::description(); ?></title>
         <link rel="stylesheet" type="text/css" href="css/main.css" />
         <script type="text/javascript" src="//code.jquery.com/jquery-1.10.2.min.js"></script>
         <script type="text/javascript" src="js/script.js"></script>
     </head>
 <body>
-    <header class="header"></header>
+    <header class="header">
+        <div>
+            <h1 class="title"><?php Site::title(); ?></h1>
+            <h2 class="description"><em><?php Site::description(); ?></em></h2>
+        </div>
+    </header>
     <div class="page">
         <?php 
         
